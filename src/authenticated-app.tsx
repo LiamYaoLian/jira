346105a2fdf8fc931901d11f3cfcabd18efc11c7
liamyaolian <liamyaolian@gmail.com>
2022-05-21 14:10:05 -0400

    style: add Row

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,22 +2,22 @@ import React from 'react';
 import {ProjectListScreen} from "./screens/project-list";
 import {useAuth} from "./context/auth-context";
 import styled from "@emotion/styled";
+import {Row} from "./components/lib";
 
 export const AuthenticatedApp = () => {
     const {logout} = useAuth()
 
 
     return <Container>
-        <Header>
-            <HeaderLeft>
-                <h3>Logo</h3>
-                <h3>Project</h3>
-                <h3>User</h3>
+        <Header between={true}>
+            <HeaderLeft gap={true}>
+                <h2>Logo</h2>
+                <h2>Project</h2>
+                <h2>User</h2>
             </HeaderLeft>
             <HeaderRight>
                 <button onClick={logout}>log out</button>
             </HeaderRight>
-
         </Header>
         <Main>
             <ProjectListScreen/>
@@ -25,10 +25,6 @@ export const AuthenticatedApp = () => {
     </Container>;
 }
 
-const PageHeader = styled.header`
-    background-color: gray;
-    height: 6rem;
-`
 
 // const Main = styled.main`
 //     height: calc(100vh - 6rem)
@@ -38,44 +34,20 @@ const PageHeader = styled.header`
 const Container = styled.div`
     display: grid;
     grid-template-rows: 6rem 1fr 6rem;
-    grid-template-columns: 20rem 1fr 20rem;
-    grid-template-areas: 
-    "header header header"
-    "nav main aside"
-    "footer footer footer";
     height: 100vh;
-    grid-gap: 10rem;
 `
 
-const Header = styled.header`
-    grid-area: header;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
+const HeaderItem = styled.h3`
+    margin-right: 3rem
 `
 
-const HeaderLeft = styled.header`
-    display: flex;
-    align-items: center;
-`
+const Header = styled(Row)``
+
+const HeaderLeft = styled(Row)``
 
 const HeaderRight = styled.header`
 
 `
 
 const Main = styled.main`
-    grid-area: main
-`
-
-const Nav = styled.nav`
-    grid-aread: nav
-`
-
-const Aside = styled.aside`
-    grid-area: aside
-`
-
-const Footer = styled.footer`
-    grid-area: footer
 `
